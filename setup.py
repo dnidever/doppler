ea60ceff5c453a0d6db202d29c45f9e90ec3c661
David Nidever <dnidever@lsst.org>
2019-10-27 23:43:07 -0600

    Using setuptools now in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='radvel',
       version='1.0',
@@ -8,7 +9,8 @@ setup(name='radvel',
       author='David Nidever',
       author_email='dnidever@montana.edu',
       url='https://github.com/dnidever/radvel',
-      packages=['radvel'],
+#      packages=['radvel'],
+      packages=find_packages(exclude=["tests"]),
 #      scripts=['bin/job_daemon'],
       requires=['numpy','astropy','scipy','thecannon','dlnpyutils']
 )
