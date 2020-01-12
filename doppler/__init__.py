552b219ffabcb17940fab914f21fcb2a8ac64457
David Nidever <dnidever@lsst.org>
2020-01-11 17:08:58 -0700

    Added reader.py module to read the spectra.

--- doppler/__init__.py
+++ doppler/__init__.py
@@ -1,8 +1,8 @@
-__all__ = ["spec1d","rv","cannon","utils"]
+__all__ = ["reader","spec1d","lsf","rv","cannon","utils"]
 
 #from .rv import SpecID
 from .spec1d import Spec1D
 from . import (rv, cannon)
 
 def read(filename=None):
-    return rv.rdspec(filename=filename)
+    return reader.read(filename=filename)
