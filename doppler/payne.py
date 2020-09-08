f037dcf53b1749ff79bbb3317826bd98c076269b
David Nidever <dnidever@lsst.org>
2020-09-07 23:23:21 -0600

    Updates to payne.py, added Payne chunk models.

--- doppler/payne.py
+++ doppler/payne.py
@@ -109,6 +109,138 @@ def load_payne_model(mfile):
     return coeffs, wavelength, labels, wavevac
 
 
+        
+    
+def prepare_payne_model(model,labels,spec,rv=None,vmacro=None,vsini=None):
+    """ Prepare a Payne spectrum for a given observed spectrum."""
+
+    
+    # Convert wavelength from air->vacuum or vice versa
+    if model.wavevac != spec.wavevac:
+        model.wavevac = spec.wavevac   # this will set things behind the scenes
+
+    # Get full wavelength range and total wavelength coverage in the orders
+    owr = dln.minmax(spec.wave)
+    owavefull = dln.valrange(spec.wave)
+    owavechunks = 0.0
+    odw = np.zeros(spec.norder,np.float64)
+    specwave = np.atleast_2d(spec.wave.copy()).T
+    for o in range(spec.norder):
+        owavechunks += dln.valrange(specwave[:,o])
+        odw[o] = np.median(dln.slope(specwave[:,o]))
+    # Get model spectrum for the entire wavelength range, across all orders
+    # Or do them separately, if there are large gaps (e.g GALAH)
+    if owavechunks > 0.5*owavefull:
+        model_all_in_one = True
+        nextend = int(np.ceil(spec.wave.size*0.10))  # extend 10% on each end
+        w0 = np.maximum( owr[0]-nextend*np.median(odw), np.min(model.dispersion))
+        w1 = np.minimum( owr[1]+nextend*np.median(odw), np.max(model.dispersion))
+        modelspec_all = model(labels,wr=[w0,w1])
+    else:
+        model_all_in_one = False
+        
+    # Observed spectrum values
+    wave = spec.wave
+    ndim = wave.ndim
+    if ndim==2:
+        npix,norder = wave.shape
+    if ndim==1:
+        norder = 1
+        npix = len(wave)
+        wave = wave.reshape(npix,norder)
+    # Loop over the orders
+    outmodel = []
+    for o in range(norder):
+        w = wave[:,o]
+        w0 = np.min(w)
+        w1 = np.max(w)
+        dw = dln.slope(w)
+        dw = np.hstack((dw,dw[-1]))
+        dw = np.abs(dw)
+        meddw = np.median(dw)
+        npix = len(w)
+
+        if (np.min(model.dispersion)>w0) | (np.max(model.dispersion)<w1):
+                raise Exception('Model does not cover the observed wavelength range')
+            
+        # Trim
+        nextend = int(np.ceil(len(w)*0.25))  # extend 25% on each end
+        nextend = np.maximum(nextend,200)    # or 200 pixels
+        if model_all_in_one == True:
+            if (np.min(model.dispersion)<(w0-nextend*meddw)) | (np.max(model.dispersion)>(w1+nextend*meddw)):            
+                gd, = np.where( (modelspec_all.wave >= (w0-nextend*meddw)) |
+                                (modelspec_all.wave <= (w1+nextend*meddw)) )
+                ngd = len(gd)
+                tmodelflux = modelspec_all.flux[gd]
+                tmodelwave = modelspec_all.wave[gd]
+            else:
+                tmodelflux = modelspec_all.flux
+                tmodelwave = modelspec_all.wave                
+        else:
+            modelspec = model(labels,wr=[w0-nextend*meddw,w1+nextend*meddw])
+            tmodelflux = modelspec.flux
+            tmodelwave = modelspec.wave
+
+        # Rebin
+        #  get LSF FWHM (A) for a handful of positions across the spectrum
+        xp = np.arange(npix//20)*20
+        fwhm = spec.lsf.fwhm(w[xp],xtype='Wave',order=o)
+        # FWHM is in units of lsf.xtype, convert to wavelength/angstroms, if necessary
+        if spec.lsf.xtype.lower().find('pix')>-1:
+            dw1 = dln.interp(w,dw,w[xp],assume_sorted=False)
+            fwhm *= dw1
+        #  convert FWHM (A) in number of model pixels at those positions
+        dwmod = dln.slope(tmodelwave)
+        dwmod = np.hstack((dwmod,dwmod[-1]))
+        xpmod = interp1d(tmodelwave,np.arange(len(tmodelwave)),kind='cubic',bounds_error=False,
+                         fill_value=(np.nan,np.nan),assume_sorted=False)(w[xp])
+        xpmod = np.round(xpmod).astype(int)
+        fwhmpix = fwhm/dwmod[xpmod]
+        # need at least ~4 pixels per LSF FWHM across the spectrum
+        #  using 3 affects the final profile shape
+        nbin = np.round(np.min(fwhmpix)//4).astype(int)
+        if nbin==0:
+            raise Exception('Model has lower resolution than the observed spectrum')
+        if nbin>1:
+            npix2 = np.round(len(tmodelflux) // nbin).astype(int)
+            rmodelflux = dln.rebin(tmodelflux[0:npix2*nbin],npix2)
+            rmodelwave = dln.rebin(tmodelwave[0:npix2*nbin],npix2)
+        else:
+            rmodelflux = tmodelflux
+            rmodelwave = tmodelwave
+                
+        # Convolve with LSF, Vsini and Vmacro kernels
+        lsf = spec.lsf.anyarray(rmodelwave,xtype='Wave',order=o,original=False)
+        cmodelflux = utils.convolve_sparse(rmodelflux,lsf)
+        # Apply Vmacro broadening and Vsini broadening (km/s)
+        if (vmacro is not None) | (vsini is not None):
+            cmodelflux = utils.broaden(rmodelwave,cmodelflux,vgauss=vmacro,vsini=vsini)
+            
+        # Apply Radial Velocity
+        if rv is not None:
+            if rv != 0.0: rmodelwave *= (1+rv/cspeed)
+        
+        # Interpolate
+        omodelflux = interp1d(rmodelwave,cmodelflux,kind='cubic',bounds_error=False,
+                              fill_value=(np.nan,np.nan),assume_sorted=True)(w)
+        
+        # Order information
+        omodel = Spec1D(omodelflux,wave=w)
+        omodel.norder = norder
+        omodel.order = o
+                
+        # Append to final output
+        outmodel.append(omodel)
+
+        
+    # Single-element list
+    if (type(outmodel) is list) & (len(outmodel)==1):
+        outmodel = outmodel[0] 
+            
+    return outmodel
+
+
+
 class PayneModel(object):
 
     def __init__(self,coeffs,wavelength,labels,wavevac=False):
@@ -269,12 +401,6 @@ class PayneModelSet(object):
     @property
     def dispersion(self):
         return self._dispersion
-
-    #@dispersion.setter
-    #def dispersion(self,disp):
-    #    if len(disp) != len(self._dispersion):
-    #        raise ValueError('Input dispersion array not of the right length')
-    #    self._dispersion = disp
     
     @property
     def wavevac(self):
@@ -418,133 +544,90 @@ class PayneModelSet(object):
 
     def prepare(self,labels,spec,rv=None,vmacro=None,vsini=None):
         return prepare_payne_model(self,labels,spec,rv=rv,vmacro=vmacro,vsini=vsini)
-        
-    
-def prepare_payne_model(model,labels,spec,rv=None,vmacro=None,vsini=None):
-    """ Prepare a Payne spectrum for a given observed spectrum."""
+
 
     
-    # Convert wavelength from air->vacuum or vice versa
-    if model.wavevac != spec.wavevac:
-        model.wavevac = spec.wavevac   # this will set things behind the scenes
+def DopplerPayneModel(object):
+    """ Thin wrapper around PayneModel or PayneModelSet."""
 
-    # Get full wavelength range and total wavelength coverage in the orders
-    owr = dln.minmax(spec.wave)
-    owavefull = dln.valrange(spec.wave)
-    owavechunks = 0.0
-    odw = np.zeros(spec.norder,np.float64)
-    specwave = np.atleast_2d(spec.wave.copy()).T
-    for o in range(spec.norder):
-        owavechunks += dln.valrange(specwave[:,o])
-        odw[o] = np.median(dln.slope(specwave[:,o]))
-    # Get model spectrum for the entire wavelength range, across all orders
-    # Or do them separately, if there are large gaps (e.g GALAH)
-    if owavechunks > 0.5*owavefull:
-        model_all_in_one = True
-        nextend = int(np.ceil(spec.wave.size*0.10))  # extend 10% on each end
-        w0 = np.maximum( owr[0]-nextend*np.median(odw), np.min(model.dispersion))
-        w1 = np.minimum( owr[1]+nextend*np.median(odw), np.max(model.dispersion))
-        modelspec_all = model(labels,wr=[w0,w1])
-    else:
-        model_all_in_one = False
+    def __init__(self,model):
+        # Make sure it's a PayneModel or PayneModelSet
+        if (not isinstance(model,PayneModel)) & (not isinstance(model,PayneModelSet)):
+            raise ValueError("Model must be either PayneModel or PayneModelSet")
+        self._data = model
+        labels = list(model.labels.copy())
+        # Add vsini, vmacro, rv
+        labels += ['vsini','vmacro','rv']
+        self._labels = labels
+        self._spec = None
+        self._prepared = False
+        self._dispersion = self._data.dispersion
+        self._wavevac = self._data._wavevac
+        self._original_wavevac = self._data._wavevac
+        self.wr = self._data.wr
+
         
-    # Observed spectrum values
-    wave = spec.wave
-    ndim = wave.ndim
-    if ndim==2:
-        npix,norder = wave.shape
-    if ndim==1:
-        norder = 1
-        npix = len(wave)
-        wave = wave.reshape(npix,norder)
-    # Loop over the orders
-    outmodel = []
-    for o in range(norder):
-        w = wave[:,o]
-        w0 = np.min(w)
-        w1 = np.max(w)
-        dw = dln.slope(w)
-        dw = np.hstack((dw,dw[-1]))
-        dw = np.abs(dw)
-        meddw = np.median(dw)
-        npix = len(w)
-
-        if (np.min(model.dispersion)>w0) | (np.max(model.dispersion)<w1):
-                raise Exception('Model does not cover the observed wavelength range')
-            
-        # Trim
-        nextend = int(np.ceil(len(w)*0.25))  # extend 25% on each end
-        nextend = np.maximum(nextend,200)    # or 200 pixels
-        if model_all_in_one == True:
-            if (np.min(model.dispersion)<(w0-nextend*meddw)) | (np.max(model.dispersion)>(w1+nextend*meddw)):            
-                gd, = np.where( (modelspec_all.wave >= (w0-nextend*meddw)) |
-                                (modelspec_all.wave <= (w1+nextend*meddw)) )
-                ngd = len(gd)
-                tmodelflux = modelspec_all.flux[gd]
-                tmodelwave = modelspec_all.wave[gd]
-            else:
-                tmodelflux = modelspec_all.flux
-                tmodelwave = modelspec_all.wave                
+    def __call__(self,labels,wr=None):
+        """ Create the model."""
+        if len(labels) != len(self.labels):
+            raise ValueError('labels must have '+str(len(self.labels))+' elements')
+        vsini,vmacro,rv = labels[-3:]
+        if self.prepared==True:
+            return self._data(labels,spec=self._spec,vsini=vsini,vmacro=vmacro,rv=rv)
         else:
-            modelspec = model(labels,wr=[w0-nextend*meddw,w1+nextend*meddw])
-            tmodelflux = modelspec.flux
-            tmodelwave = modelspec.wave
+            return self._data(label,vsini=vsini,vmacro=vmacro,rv=rv)        
 
-        # Rebin
-        #  get LSF FWHM (A) for a handful of positions across the spectrum
-        xp = np.arange(npix//20)*20
-        fwhm = spec.lsf.fwhm(w[xp],xtype='Wave',order=o)
-        # FWHM is in units of lsf.xtype, convert to wavelength/angstroms, if necessary
-        if spec.lsf.xtype.lower().find('pix')>-1:
-            dw1 = dln.interp(w,dw,w[xp],assume_sorted=False)
-            fwhm *= dw1
-        #  convert FWHM (A) in number of model pixels at those positions
-        dwmod = dln.slope(tmodelwave)
-        dwmod = np.hstack((dwmod,dwmod[-1]))
-        xpmod = interp1d(tmodelwave,np.arange(len(tmodelwave)),kind='cubic',bounds_error=False,
-                         fill_value=(np.nan,np.nan),assume_sorted=False)(w[xp])
-        xpmod = np.round(xpmod).astype(int)
-        fwhmpix = fwhm/dwmod[xpmod]
-        # need at least ~4 pixels per LSF FWHM across the spectrum
-        #  using 3 affects the final profile shape
-        nbin = np.round(np.min(fwhmpix)//4).astype(int)
-        if nbin==0:
-            raise Exception('Model has lower resolution than the observed spectrum')
-        if nbin>1:
-            npix2 = np.round(len(tmodelflux) // nbin).astype(int)
-            rmodelflux = dln.rebin(tmodelflux[0:npix2*nbin],npix2)
-            rmodelwave = dln.rebin(tmodelwave[0:npix2*nbin],npix2)
+    @property
+    def wavevac(self):
+        return self._data.wavevac
+    
+    @wavevac.setter
+    def wavevac(self,wavevac):
+        """ Set wavelength wavevac value."""
+        self._data.wavevac = wavevac
+
+    @property
+    def prepared:
+        return self._prepared
+
+    @prepared.setter
+    def prepared(self,value):
+        """ Can be used to unprepare() the model."""
+        if (value==0) | (value==False):
+            self.unprepare()
+
+    def unprepare(self):
+        """ Used to unprepare the model."""
+        self._spec = None
+        self._prepared = False
+        self._dispersion = self._data.dispersion   
+        self._data.wavevac = self._original_wavevac  # reset to original
+        self._wavevac = self._original_wavevac
+        self.wr = self._data.wr
+
+        
+    def prepare(self,spec):
+        """ Prepare the model.  Keep a copy of the spectrum."""
+        self._spec = spec.copy()
+        self._prepared = True
+        self._dispersion = spec.wave.copy()
+        self._data.wavevac = spec.wavevac   # switch air<->vac if necessary
+        self._wavevac = spec.wavevac
+        self.wr = dln.minmax(self._dispersion)
+
+    def copy(self):
+        """ Make a copy of the DopplerPayneModel."""
+        new_model = self._data.copy()
+        new = DopplerPayneModel(new_model)
+        if self.prepared==True:
+            new.prepare(self._spec)
+        return new
+
+    def read(mfiles):
+        """ Read a set of Payne model files."""
+        if dln.size(mfiles)==1:
+            model = PayneModel.read(mfiles)
         else:
-            rmodelflux = tmodelflux
-            rmodelwave = tmodelwave
-                
-        # Convolve with LSF, Vsini and Vmacro kernels
-        lsf = spec.lsf.anyarray(rmodelwave,xtype='Wave',order=o,original=False)
-        cmodelflux = utils.convolve_sparse(rmodelflux,lsf)
-        # Apply Vmacro broadening and Vsini broadening (km/s)
-        if (vmacro is not None) | (vsini is not None):
-            cmodelflux = utils.broaden(rmodelwave,cmodelflux,vgauss=vmacro,vsini=vsini)
-            
-        # Apply Radial Velocity
-        if rv is not None:
-            if rv != 0.0: rmodelwave *= (1+rv/cspeed)
-        
-        # Interpolate
-        omodelflux = interp1d(rmodelwave,cmodelflux,kind='cubic',bounds_error=False,
-                              fill_value=(np.nan,np.nan),assume_sorted=True)(w)
-
-        
-        # Order information
-        omodel = Spec1D(omodelflux,wave=w)
-        omodel.norder = norder
-        omodel.order = o
-                
-        # Append to final output
-        outmodel.append(omodel)
-
-        
-    # Single-element list
-    if (type(outmodel) is list) & (len(outmodel)==1):
-        outmodel = outmodel[0] 
-            
-    return outmodel
+            model = PayneModelSet.read(mfiles)
+        return DopplerPaynModel(model)
+    
