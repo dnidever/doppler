b5aac2ec8fa9635a09d8f9bd87d81c346282713a
David Nidever <dnidever@lsst.org>
2022-11-08 22:12:38 -0700

    fixed bug in setup.py file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Doppler',
-      version='1.1.2',
+      version='1.1.3',
       description='Generic Radial Velocity Software',
       author='David Nidever',
       author_email='dnidever@montana.edu',
@@ -12,7 +12,7 @@ setup(name='Doppler',
       packages=find_packages(exclude=["tests"]),
       scripts=['bin/doppler'],
       install_requires=['numpy','astropy(>=4.0)','scipy','dlnpyutils(>=1.0.3)','dill','emcee','corner',
-                        'annieslasso']
+                        'annieslasso'],
       #dependency_links=['http://github.com/dnidever/AnniesLasso/tarball/v1.0.0#egg=the-cannon'],      
-      include_package_data=True,
+      include_package_data=True
 )
