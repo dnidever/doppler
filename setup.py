dca650c475ca730406d6830e4b0accd0de88d732
David Nidever <dnidever@montana.edu>
2025-10-15 15:26:06 -0600

    bumped version from 1.1.28 to 1.1.29

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 pypiname = 'thedoppler'
 
 setup(name="doppler",
-      version='1.1.28',
+      version='1.1.29',
       description='Generic Radial Velocity Software',
       author='David Nidever',
       author_email='dnidever@montana.edu',
